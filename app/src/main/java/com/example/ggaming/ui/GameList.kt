418d323d8f81423b1bb3bad80a8a3801b1e68e27
Joviar <joviar27@gmail.com>
2024-10-12 07:44:05 +0700

    feat: Create game list view

---
+++ app/src/main/java/com/example/ggaming/ui/GameList.kt
@@ -0,0 +1,38 @@
+package com.example.ggaming.ui
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.core.domain.model.Game
+import com.example.core.domain.model.createDummyGameList
+
+@Composable
+fun GameList(
+    gameList: List<Game>,
+    event: (GameEvent) -> Unit
+) {
+    LazyColumn(
+        Modifier.padding(8.dp),
+        verticalArrangement = Arrangement.spacedBy(8.dp)
+    ) {
+        items(gameList){
+            GameItem(
+                game = it,
+                event = event
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GameListPreview(){
+    GameList(
+        gameList = createDummyGameList(3)
+    ){ }
+}
