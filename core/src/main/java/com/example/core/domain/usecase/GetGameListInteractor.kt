75e090c630329d555955c46cd73172da3ad39531
Joviar <joviar27@gmail.com>
2024-10-14 16:46:25 +0700

    feat: add logger in get game list interactor

--- core/src/main/java/com/example/core/domain/usecase/GetGameListInteractor.kt
+++ core/src/main/java/com/example/core/domain/usecase/GetGameListInteractor.kt
@@ -1,5 +1,6 @@
 package com.example.core.domain.usecase
 
+import android.util.Log
 import androidx.paging.PagingData
 import com.example.core.Result
 import com.example.core.domain.model.Game
@@ -7,7 +8,7 @@ import com.example.core.domain.repository.IGameRepository
 import kotlinx.coroutines.flow.first
 import javax.inject.Inject
 
-class GameInteractor @Inject constructor(
+class GetGameListInteractor @Inject constructor(
     private val gameRepository: IGameRepository
 ): GetGameListUseCase {
     override suspend fun getGameList(query: String?): Result<PagingData<Game>> {
@@ -15,6 +16,7 @@ class GameInteractor @Inject constructor(
             val gameList = gameRepository.getGameList(query).first()
             return Result.Success(gameList)
         }catch (e: Exception){
+            Log.d("error", "${this.javaClass.simpleName}: ${e.message}")
             return Result.Error(e.message.toString())
         }
     }
